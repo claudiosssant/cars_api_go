03c0c1958e5c5a28fe606e0905104bf59b44e780
Claudio <claudiogsssantos@gmail.com>
2024-11-17 23:03:37 -0300

    feat: criando controller e model para cadastro de carro

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"cars_api/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
+	carController := controller.NewCarController()
+
 	server.GET("/ping", func (ctx *gin.Context) {
 		ctx.JSON(200, gin.H {
 			"message": "TESTE",
 		})
 	})
+
+	server.GET("/cars", carController.GetCar)
 
 	server.Run(":3333")
 }
